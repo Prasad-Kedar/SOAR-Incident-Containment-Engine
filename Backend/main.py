21a72b7672ab6e0ae97e9d4fd3456ed3ba2eca71
Prasad-Kedar <prasadkedar04@gmail.com>
2026-06-07 09:51:14 +0530

    implemented alert ingestion API

--- Backend/main.py
+++ Backend/main.py
@@ -7,3 +7,28 @@ def home():
     return {
         "message":"SOAR Engine Running"
     }
+
+from fastapi import FastAPI
+from models import Alert
+from alerts import alerts_db
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "SOAR Engine Running"}
+
+@app.post("/alerts")
+def receive_alert(alert: Alert):
+
+    alerts_db.append(alert.dict())
+
+    return {
+        "status": "success",
+        "message": "Alert received",
+        "total_alerts": len(alerts_db)
+    }
+
+@app.get("/alerts")
+def get_alerts():
+    return alerts_db
